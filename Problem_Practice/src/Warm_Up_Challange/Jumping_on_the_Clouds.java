59721b2bd6187f76ad0ee6893dc091e7cff6f668
Rafa2016831028 <jarintasnimaugust20@gmail.com>
2021-01-30 01:42:45 +0600

    3rd problem not completed
    3rd problem not completed

---
+++ Problem_Practice/src/Warm_Up_Challange/Jumping_on_the_Clouds.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Warm_Up_Challange;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Asus
+ */
+public class Jumping_on_the_Clouds {
+    public static void main(String[] args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i=0 ; i<n; i++){
+            a[i] =sc.nextInt();
+        }
+        int count =0;
+        for(int i=0; i<a.length-2; i++){
+            if(a[i]==0){
+               
+            
+            }
+        }
+        System.out.println(count);
+    }
+}
